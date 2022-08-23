fdaffd72662c99c788d49a6292b09b81a3377376
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-08-23 10:53:10 +0500

    Commit

--- auth/settings.py
+++ auth/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'auth.wsgi.application'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
